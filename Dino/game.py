2b1e2c32f6e879dc8fa74af52dc04929a9ff867f
pyguru123 <pyguru123@gmail.com>
2021-02-17 17:08:27 +0530

    updated dino

--- Dino/game.py
+++ Dino/game.py
@@ -1,7 +1,9 @@
+import math
+import random
 import pygame
-from pygame.locals import QUIT, KEYDOWN, K_SPACE
+from pygame.locals import QUIT, KEYDOWN, K_SPACE, K_UP, MOUSEBUTTONUP
 
-from objects import Dino, Cactus, Cloud
+from objects import Dino, Cactus, Cloud, Ptera, GameOptions
 
 ### SETUP *********************************************************************
 SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 700, 420
@@ -11,75 +13,168 @@ pygame.init()
 clock = pygame.time.Clock()
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Dino')
+pygame.mixer.music.load('music/Super_Sigil_Quest_for_the_Celestial_Dinosaur.mp3')
+pygame.mixer.music.play(loops=-1)
 
 bg = pygame.image.load('assets/bg1.jpg').convert()
 bgX = 0
 bgX2 = bg.get_width()
 
+font = pygame.font.Font('freesansbold.ttf', 32)
+dodgerblue = (30, 144, 255)
+checkpoint_sound = pygame.mixer.Sound("music/checkPoint.wav")
+
 ### Objects & Events **********************************************************
 ADDCACTUS = pygame.USEREVENT + 1
 ADDCLOUD = pygame.USEREVENT + 2
-pygame.time.set_timer(ADDCACTUS, 5000)
-pygame.time.set_timer(ADDCLOUD, 8000)
+ADDPTERA = pygame.USEREVENT + 3
+cactus_time = random.choice([5000,6000,7000])
+cloud_time  = 8000
+ptera_time = 23000
+pygame.time.set_timer(ADDCACTUS, cactus_time)
+pygame.time.set_timer(ADDCLOUD, cloud_time)
+pygame.time.set_timer(ADDPTERA, ptera_time)
 
 cactus_sprites = pygame.sprite.Group()
 cloud_sprites = pygame.sprite.Group()
+ptera_sprites = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
+dinos = pygame.sprite.Group()
 
-dino = Dino()
-dinos = pygame.sprite.Group(dino)
+game_options = GameOptions()
 
-running = True
-gameUpdating = True
-while running:
-	collision = 0
+### Main Game *****************************************************************
+if __name__ == '__main__':
+	gameUpdating = False
+	gameStarted = False
+	isDead = False
+	gameOver = False
 
-	win.blit(bg,(bgX,0))
-	win.blit(bg,(bgX2,0))
+	high_score = 0
+	new_score = 0
 
-	if gameUpdating:
-		bgX -= 1.8
-		bgX2 -= 1.8
-		if bgX < bg.get_width() * -1:
-			bgX = bg.get_width()
-		if bgX2 < bg.get_width() * -1:
-			bgX2 = bg.get_width()
+	running = True
+	while running:
+		collision = 0
 
+		win.blit(bg,(bgX,0))
+		win.blit(bg,(bgX2,0))
 
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			running = False
+		if not gameUpdating:
+			score = 0
+			mask1, mask2 = None, None
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					running = False
+			
+				if event.type == MOUSEBUTTONUP:
+					pos = pygame.mouse.get_pos()
+					mask1 = 504 < pos[0] < 531 and 181 < pos[1] < 206
 
-		if event.type == ADDCACTUS:
-			c = Cactus()
-			cactus_sprites.add(c)
-			all_sprites.add(c)
-		if event.type == ADDCLOUD:
-			c = Cloud()
-			cloud_sprites.add(c)
-			all_sprites.add(c)
+				if event.type == KEYDOWN:
+					mask2 = event.key == K_SPACE or event.key == K_UP
 
-	pressed_keys = pygame.key.get_pressed()
-	dinos.update(pressed_keys)
+				if mask1 or mask2:
+						cactus_sprites.empty()
+						cloud_sprites.empty()
+						ptera_sprites.empty()
+						all_sprites.empty()
+						dinos.empty()
 
-	if gameUpdating:
-		cactus_sprites.update()
-		cloud_sprites.update()
+						dino = Dino()
+						dinos.add(dino)
+						gameUpdating = True
+						gameStarted = True
+						isDead = False
+						gameOver = False
 
-	for entity in all_sprites:
-		win.blit(entity.surf, entity.rect)
-		pygame.draw.rect(win, (255,0,0), entity.rect, 2)
-	dinos.draw(win)
+						new_score = 0
+						score = 0
+						speed = -5
+						pygame.time.delay(500)
 
-	for entity in cactus_sprites:
-		collision = dino.is_collided_with(entity)
-		if collision:
-			dino.is_dead()
-			gameUpdating = False
+				if event.type == MOUSEBUTTONUP:
+					print(pygame.mouse.get_pos())
+		else:
+			bgX -= 1.8
+			bgX2 -= 1.8
+			if bgX < bg.get_width() * -1:
+				bgX = bg.get_width()
+			if bgX2 < bg.get_width() * -1:
+				bgX2 = bg.get_width()
 
+		if gameStarted:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					running = False
 
+				if event.type == ADDCLOUD:
+					c = Cloud()
+					cloud_sprites.add(c)
+					all_sprites.add(c)
+				if event.type == ADDCACTUS:
+					if len(ptera_sprites) < 1:
+						c = Cactus()
+						cactus_sprites.add(c)
+						all_sprites.add(c)
+				if event.type == ADDPTERA:
+					t = Ptera()
+					ptera_sprites.add(t)
+					all_sprites.add(t)
 
-	pygame.display.flip()
-	clock.tick(30)
+			pressed_keys = pygame.key.get_pressed()
+			dinos.update(pressed_keys)
 
-pygame.quit()
+			if gameUpdating:
+				cactus_sprites.update(speed)
+				cloud_sprites.update(speed)
+				ptera_sprites.update(speed)
+
+			for entity in all_sprites:
+				win.blit(entity.surf, entity.rect)
+			dinos.draw(win)
+
+			if not isDead:
+				for entity in cactus_sprites:
+					collision = dino.is_collided_with(entity)
+					if collision:
+						dino.is_dead()
+						isDead = True
+						gameOver = True
+						gameUpdating = False
+
+				for entity in ptera_sprites:
+					if pygame.sprite.spritecollideany(dino, ptera_sprites):
+						if entity.rect.left < dino.rect.left < entity.rect.right - 20:
+							dino.is_dead()
+							isDead = True
+							gameOver = True
+							gameUpdating = False
+
+				score += 0.25
+				new_score = math.floor(score)
+				if new_score > 0 and new_score % 100 == 0:
+					checkpoint_sound.play()
+					speed -= 0.5
+
+				if new_score >= high_score:
+					high_score = new_score
+
+			text = font.render('Score : ' + str(new_score), 1, dodgerblue)
+			win.blit(text, (40, 10))
+			text = font.render('High Score : ' + str(high_score), 1, dodgerblue)
+			win.blit(text, (420, 10))
+
+			if gameOver:
+				win.blit(game_options.game_over, game_options.game_over_rect)
+				win.blit(game_options.replay, game_options.replay_rect)
+
+		else:
+			win.blit(game_options.dino_wall, game_options.dino_rect)
+			win.blit(game_options.dino_run, game_options.dino_run_rect)
+			win.blit(game_options.start, game_options.start_rect)
+
+		pygame.display.flip()
+		clock.tick(30)
+
+	pygame.quit()
--- Dino/objects.py
+++ Dino/objects.py
@@ -1,7 +1,11 @@
 import math
 import random
 import pygame
-from pygame.locals import (RLEACCEL, K_SPACE)
+from pygame.locals import (RLEACCEL, K_SPACE, K_UP, K_DOWN)
+
+pygame.mixer.init()
+jump_sound = pygame.mixer.Sound("music/jump.wav")
+die_sound = pygame.mixer.Sound("music/die1.flac")
 
 class Dino(pygame.sprite.Sprite):
 	def __init__(self):
@@ -9,15 +13,18 @@ class Dino(pygame.sprite.Sprite):
 
 		self.isRunning = True
 		self.isJumping = False
+		self.isDucking = False
 		self.isDead = False
 
 		self.runlist = [f'assets/dino/Run{i+1}.png' for i in range(8)]
 		self.jumplist = [f'assets/dino/Jump{i+1}.png' for i in range(12)]
 		self.deadlist = [f'assets/dino/Dead{i+1}.png' for i in range(8)]
+		self.ducklist = [f'assets/dino/Duck{i+1}.png' for i in range(10)]
 
 		self.runindex = 0
 		self.jumpindex = 0
 		self.deadindex = 0
+		self.duckindex = 0
 
 		self.jumpcount = 0
 		self.jumpvalues = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,
@@ -27,7 +34,7 @@ class Dino(pygame.sprite.Sprite):
 						  -2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,
 						  -3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
 
-		self.hitdict = {0:90, 1:80, 2:80, 3:80 }
+		self.hitdict = {0:85, 1:85, 2:85, 3:85 }
 
 		self.image = pygame.image.load(self.runlist[self.runindex]).convert()
 		self.image.set_colorkey((0,0,0), RLEACCEL)
@@ -36,11 +43,22 @@ class Dino(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect(center=(self.x, self.y))
 
 	def update(self, pressed_keys):
-		if pressed_keys[K_SPACE] and not self.isDead:
-			if not self.isJumping:
-				self.isJumping = True
-				self.jumpindex = 0
+		if (pressed_keys[K_SPACE] or pressed_keys[K_UP]):
+			if not self.isDead and not self.isDucking:
+				if not self.isJumping:
+					self.isJumping = True
+					self.jumpindex = 0
+					self.isRunning = False
+					jump_sound.play()
+
+		elif pressed_keys[K_DOWN] and not self.isDead and not self.isJumping:
+			if not self.isDucking:
+				self.isDucking = True
+				self.isJumping = False
 				self.isRunning = False
+				self.jumpindex = 0
+				self.runindex = 0
+				self.duckindex = 0
 				
 		if self.isJumping:
 			self.jumpindex += 0.20
@@ -59,8 +77,24 @@ class Dino(pygame.sprite.Sprite):
 				self.rect = self.image.get_rect(center=(self.x, self.y))
 
 				self.jumpcount += 2
+
+		elif self.isDucking:
+			self.rect = self.image.get_rect(center=(self.x, 350))
+			self.duckindex += 0.20
+			if self.duckindex > len(self.ducklist) - 1:
+				self.duckindex = 0
+				self.runindex = 0
+				self.isDucking = False
+				self.isRunning = True
+				self.rect = self.image.get_rect(center=(self.x, 335))
+			index = math.floor(self.duckindex)
+			img = self.ducklist[index]
+			self.image = pygame.image.load(img)
+
 		
 		elif self.isRunning:
+			if self.rect.bottom != 390:
+				self.rect.move(130,390)
 			self.runindex += 0.40
 			if self.runindex > len(self.runlist) - 1:
 				self.runindex = 0
@@ -69,12 +103,15 @@ class Dino(pygame.sprite.Sprite):
 			self.image = pygame.image.load(img)
 
 		elif self.isDead:
-			self.deadindex += 0.20
-			if self.deadindex > len(self.deadlist) - 1:
-				self.deadindex = len(self.deadlist) - 1
-			index = math.floor(self.deadindex)
-			img = self.deadlist[index]
-			self.image = pygame.image.load(img)
+			if self.rect.bottom < 390:
+				self.rect.move_ip(2, 5)
+			else:
+				self.deadindex += 0.20
+				if self.deadindex > len(self.deadlist) - 1:
+					self.deadindex = len(self.deadlist) - 1
+				index = math.floor(self.deadindex)
+				img = self.deadlist[index]
+				self.image = pygame.image.load(img)
 
 	def is_collided_with(self, sprite):
 		distance = self.hitdict[sprite.type]
@@ -89,7 +126,31 @@ class Dino(pygame.sprite.Sprite):
 		self.isJumping = False
 		self.isRunning = False
 		self.isDead = True
+		die_sound.play()
 
+class Ptera(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Ptera, self).__init__()
+
+		self.pteralist = [f'assets/Ptera/t{i+1}.png' for i in range(4)]
+		self.index = 0
+		xpos = random.choice(range(830, 930))
+
+		self.surf = pygame.image.load(self.pteralist[self.index]).convert()
+		self.surf.set_colorkey((0,0,0), RLEACCEL)
+		self.rect = self.surf.get_rect(center=(xpos,272))
+
+	def update(self, speed):
+		self.rect.move_ip(speed,0)
+		if self.rect.right < 0:
+			self.kill()
+
+		self.index += 0.20
+		if self.index > len(self.pteralist) - 1:
+			self.index = 0
+		index = math.floor(self.index)
+		img = self.pteralist[index]
+		self.surf = pygame.image.load(img)
 
 
 class Cactus(pygame.sprite.Sprite):
@@ -99,14 +160,16 @@ class Cactus(pygame.sprite.Sprite):
 		cactuslist = [f'assets/Cactus/c{i+1}.png' for i in range(4)]
 		bg = random.choice(cactuslist)
 		self.type = cactuslist.index(bg)
-		xpos = random.choice(range(900, 1000))
+		posdict = {0:340, 1:325, 2:340, 3:325}
+		ypos = posdict[self.type]
+		xpos = random.choice(range(730, 830))
 
 		self.surf = pygame.image.load(bg).convert()
 		self.surf.set_colorkey((0,0,0), RLEACCEL)
-		self.rect = self.surf.get_rect(center=(xpos,345))
+		self.rect = self.surf.get_rect(center=(xpos,ypos))
 
-	def update(self):
-		self.rect.move_ip(-5,0)
+	def update(self, speed):
+		self.rect.move_ip(speed,0)
 		if self.rect.right < 0:
 			self.kill()
 
@@ -122,7 +185,33 @@ class Cloud(pygame.sprite.Sprite):
 		self.surf.set_colorkey((0,0,0), RLEACCEL)
 		self.rect = self.surf.get_rect(center=(xpos,100))
 
-	def update(self):
-		self.rect.move_ip(-5,0)
+	def update(self, speed):
+		self.rect.move_ip(speed,0)
 		if self.rect.right < 0:
 			self.kill()
+
+class GameOptions(pygame.sprite.Sprite):
+	def __init__(self):
+		super(GameOptions, self).__init__()
+
+		self.dino_wall = pygame.image.load('assets/dino-wall.png').convert()
+		self.dino_wall.set_colorkey((0,0,0), RLEACCEL)
+		self.dino_rect = self.dino_wall.get_rect(center=(200,230))
+
+		self.dino_run = pygame.image.load('assets/dino_run.png')
+		self.dino_run_rect = self.dino_run.get_rect(center=(520,110))
+
+		self.start = pygame.image.load('assets/game_start.png').convert()
+		self.start.set_colorkey((0,0,0), RLEACCEL)
+		self.start_rect = self.dino_run.get_rect(center=(500,330))
+
+		self.game_over = pygame.image.load('assets/game_over.png').convert()
+		self.game_over.set_colorkey((0,0,0), RLEACCEL)
+		self.game_over_rect = self.game_over.get_rect(center=(350,150))
+
+		self.replay = pygame.image.load('assets/replay.png').convert()
+		self.replay.set_colorkey((0,0,0), RLEACCEL)
+		self.replay_rect = self.replay.get_rect(center=(380,280))
+
+	def update(self, mode='start'):
+		pass
