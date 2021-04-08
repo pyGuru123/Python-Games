a802ebcde1f243e5df4b7524b12cb4c015983565
pyguru123 <pyguru123@gmail.com>
2021-04-08 10:44:45 +0530

    added info cards

--- Memory Puzzle/game.py
+++ Memory Puzzle/game.py
@@ -1,18 +1,18 @@
-import os
+import json
 import pygame
-from objects import Board
+from objects import Board, Button, message_box
 
 ### SETUP *********************************************************************
 pygame.init()
-SCREEN = WIDTH, HEIGHT = 740, 520
-win = pygame.display.set_mode(SCREEN)
+SCREEN = WIDTH, HEIGHT = 890, 480
+win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
 pygame.display.set_caption('Memory Puzzle')
 
 clock = pygame.time.Clock()
 FPS = 30
 
 ROWS, COLS = 8, 10
-TILESIZE = 50
+TILESIZE = 45
 
 ### COLORS ********************************************************************
 BLACK = (0, 0, 0)
@@ -22,102 +22,171 @@ WHITE = (255, 255, 255)
 
 ### LOADING IMAGES ************************************************************
 img_list = []
-for img in os.listdir('Assets/icons/'):
-	image = pygame.image.load('Assets/icons/' + img)
+for img in range(1,21):
+	image = pygame.image.load(f"Assets/icons/{img}.jpeg")
 	image = pygame.transform.scale(image, (TILESIZE,TILESIZE))
 	img_list.append(image)
 
 bg = pygame.image.load('Assets/bg.jpg')
+rightbar = pygame.image.load('Assets/image.jpg')
+rightbar = pygame.transform.scale(rightbar, (280, HEIGHT - 47))
+
+### Buttons *******************************************************************
+restart_img = pygame.image.load('Assets/restart.png')
+restart_btn = Button(restart_img, (40,40), 720, 230)
+
+info_img = pygame.image.load('Assets/info.png')
+info_btn = Button(info_img, (40,40), 720, 280)
+
+close_img = pygame.image.load('Assets/close.png')
+close_btn = Button(close_img, (40,40), 720, 330)
+
+### LOADING FRUITS INFORMATION ************************************************
+with open('Info/info.json') as f:
+	dct = json.load(f)
+
+### LOADING FONTS *************************************************************
+sys_font = pygame.font.SysFont(("Times New Roman"),20)
 
 ### CREATING BOARD ************************************************************
 board = Board(img_list)
-
+board.randomize_images()
 
 ### GAME VARIABLES ************************************************************
+game_screen = True
 first_card = None
 second_card = None
 first_click_time = None
 second_click_time = None
 
 running = True
+
 while running:
-	win.blit(bg, (0,0))
+	win.blit(bg, (0,0), (400, 100,WIDTH,HEIGHT))
+	win.blit(rightbar, (595, 20))
+	pygame.draw.rect(win, BLUE, (5, 10, 580, HEIGHT - 20), 2)
+	pygame.draw.rect(win, BLUE, (585, 10, 300, HEIGHT - 20), 2)
+
+	if restart_btn.draw(win):
+		game_screen = True
+		show_text = False
+		first_card = None
+		second_card = None
+		first_click_time = None
+		second_click_time = None
+
+		board.randomize_images()
+
+	if info_btn.draw(win):
+		game_screen = False
+		show_text = False
+
+	if close_btn.draw(win):
+		running = False
+
 	clicked = False
 
 	x, y = pygame.mouse.get_pos()
 
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if event.button == 1:
 				clicked = True
 				x, y = pygame.mouse.get_pos()
 
-	### Polling time to hide cards
-	if second_click_time:
-		current_time = pygame.time.get_ticks()
+	if game_screen:
+		### Game is on
 
-		delta = current_time - second_click_time
-		if delta >= 1000:
-			if first_card.value == second_card.value:
-				first_card.is_alive = False
-				second_card.is_alive = False
+		### Polling time to hide cards
+		if second_click_time:
+			current_time = pygame.time.get_ticks()
 
-			index = first_card.index
-			fcard = board.board[index[0]][index[1]]
-			fcard.animate = True
-			fcard.slide_left = False
-			first_card = None
+			delta = current_time - second_click_time
+			if delta >= 1000:
+				if first_card.value == second_card.value:
+					first_card.is_alive = False
+					second_card.is_alive = False
 
-			index = second_card.index
-			scard = board.board[index[0]][index[1]]
-			scard.animate = True
-			scard.slide_left = False
-			second_card = None
+				index = first_card.index
+				fcard = board.board[index[0]][index[1]]
+				fcard.animate = True
+				fcard.slide_left = False
+				first_card = None
 
-			first_click_time = None
-			second_click_time = None
-		else:
-			clicked = False
+				index = second_card.index
+				scard = board.board[index[0]][index[1]]
+				scard.animate = True
+				scard.slide_left = False
+				second_card = None
 
-	### Displaying cards
-	for r in range(ROWS):
-		for c in range(COLS):
-			border = False
-			card = board.board[r][c]
-			if card.is_alive:
+				first_click_time = None
+				second_click_time = None
+			else:
+				clicked = False
+
+		### Displaying cards
+		for r in range(ROWS):
+			for c in range(COLS):
+				border = False
+				card = board.board[r][c]
+				if card.is_alive:
+					xcord = card.rect.x
+					ycord = card.rect.y
+
+					if card.rect.collidepoint((x,y)):
+						border = True
+						if clicked:
+							card.visible = True
+							card.animate = True
+							card.slide_left = True
+
+							if not first_card:
+								first_card = card
+							else:
+								second_card = card
+								if second_card != first_card:
+									second_click_time = pygame.time.get_ticks()
+								else:
+									second_card = None
+
+					pygame.draw.rect(win, BLACK, (xcord+5, ycord+5,TILESIZE, TILESIZE))
+
+					if not card.animate:
+						if card.visible:
+							win.blit(card.image, card.rect)
+						else:
+							pygame.draw.rect(win, WHITE, (xcord, ycord,TILESIZE, TILESIZE))
+
+						if border:
+							pygame.draw.rect(win, RED, (xcord, ycord,TILESIZE, TILESIZE), 2)
+					else:
+						card.on_click(win)
+	else:
+		for r in range(2):
+			for c in range(COLS):
+				card = board.info_board[r][c]
 				xcord = card.rect.x
 				ycord = card.rect.y
 
-				if card.rect.collidepoint((x,y)):
-					border = True
-					if clicked:
-						card.visible = True
-						card.animate = True
-						card.slide_left = True
-
-						if not first_card:
-							first_card = card
-						else:
-							second_card = card
-							if second_card != first_card:
-								second_click_time = pygame.time.get_ticks()
-							else:
-								second_card = None
-
 				pygame.draw.rect(win, BLACK, (xcord+5, ycord+5,TILESIZE, TILESIZE))
+				win.blit(card.image, card.rect)
 
-				if not card.animate:
-					if card.visible:
-						win.blit(card.image, card.rect)
-					else:
-						pygame.draw.rect(win, WHITE, (xcord, ycord,TILESIZE, TILESIZE))
+				if card.rect.collidepoint((x,y)):
+					pygame.draw.rect(win, RED, (xcord, ycord,TILESIZE, TILESIZE), 2)
 
-					if border:
-						pygame.draw.rect(win, RED, (xcord, ycord,TILESIZE, TILESIZE), 2)
-				else:
-					card.on_click(win)
+					if clicked:
+						show_text = True
+						data = dct[str(card.value)]
+						name = data['Name']
+						info = data['Info']
+
+						border = True
+						pos = (xcord, ycord)
+
+		if border:
+			pygame.draw.rect(win, BLUE, (pos[0], pos[1],TILESIZE, TILESIZE), 2)
+		if show_text:
+			message_box(win, sys_font, info)
 
 	pygame.display.flip()
 	clock.tick(FPS)
--- Memory Puzzle/objects.py
+++ Memory Puzzle/objects.py
@@ -3,14 +3,15 @@ import pygame
 
 SCREEN = WIDTH, HEIGHT = 640, 520
 ROWS, COLS = 8, 10
-TILESIZE = 50
+TILESIZE = 45
 
 class Board:
 	def __init__(self, imglist):
 		self.image_list = imglist
 		self.extended_imglist = 4 * [i for i in range(1, len(self.image_list) + 1)]
-		
-		self.board = self.randomize_images()
+
+		self.board = None
+		self.info_board = self.info_cards()
 
 	def randomize_images(self):
 		board = [[0 for j in range(COLS)] for i in range(ROWS)]
@@ -25,6 +26,20 @@ class Board:
 				x = c*TILESIZE + c * 10 + 20
 				y = r*TILESIZE + r * 10 + 20
 				card = Card(value, (r,c), image, (x, y))
+				board[r][c] = card
+
+		self.board = board
+
+	def info_cards(self):
+		board = [[0 for i in range(10)] for j in range(2)]
+
+		for r in range(2):
+			for c in range(COLS):
+				value = r * COLS + c + 1
+				image = self.image_list[value-1]
+				x = c*TILESIZE + c * 10 + 20
+				y = r*TILESIZE + r * 10 + 20
+				card = InfoCard(value, (r,c), image, (x, y))
 				board[r][c] = card
 
 		return board
@@ -51,16 +66,69 @@ class Card:
 		if self.visible:
 			if self.slide_left:
 				if self.cover_x > 0:
-					self.cover_x -= 5
-				if self.cover_x == 0:
+					self.cover_x -= 8
+				if self.cover_x <= 0:
 					self.animate = False
 			else:
 				if self.cover_x < TILESIZE:
-					self.cover_x += 5
-				if self.cover_x == TILESIZE:
+					self.cover_x += 8
+				if self.cover_x >= TILESIZE:
 					self.animate = False
 					self.visible = False
 
 			win.blit(self.image, self.rect)
 			rect =  (self.rect.x, self.rect.y, self.cover_x, TILESIZE)
 			pygame.draw.rect(win, (255, 255, 255), rect)
+
+class InfoCard:
+	def __init__(self, value, index, image, pos):
+		self.value = value
+		self.index = index
+		self.image = image
+		self.pos = pos
+
+		self.rect = self.image.get_rect()
+		self.rect.x = self.pos[0]
+		self.rect.y = self.pos[1]
+
+class Button(pygame.sprite.Sprite):
+	def __init__(self, img, scale, x, y):
+		super(Button, self).__init__()
+
+		self.image = pygame.transform.scale(img, scale)
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+		self.clicked = False
+
+	def draw(self, win, image=None):
+		if image:
+			self.image = image
+		action = False
+		pos = pygame.mouse.get_pos()
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] and not self.clicked:
+				action = True
+				self.clicked = True
+
+			if not pygame.mouse.get_pressed()[0]:
+				self.clicked = False
+
+		win.blit(self.image, self.rect)
+		return action
+
+def message_box(win, font, text):
+	WIDTH = 540
+	HEIGHT = 300
+	x = 30
+	y = 185 # depends on message box location
+	pygame.draw.rect(win, (255,255,255), (25, 150, WIDTH, HEIGHT), border_radius=10)
+	for word in text.split(' '):
+		rendered = font.render(word, 0, (0,0,0))
+		width = rendered.get_width()
+		if x + width >= WIDTH:
+			x = 30
+			y += 25
+		win.blit(rendered, (x, y))
+		x += width + 5
