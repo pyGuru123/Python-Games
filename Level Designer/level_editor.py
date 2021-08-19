a8537f8ded96dcb21d59e1f51cf82362663e5772
pyguru123 <pyguru123@gmail.com>
2021-08-19 09:56:32 +0530

    updated level designer

--- Level Designer/level_editor.py
+++ Level Designer/level_editor.py
@@ -6,23 +6,27 @@ import button
 pygame.init()
 
 # game window
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 384
 MARGIN_LEFT = 300
 WIDTH = SCREEN_WIDTH + MARGIN_LEFT
 HEIGHT = SCREEN_HEIGHT
 
-ROWS = 16
-MAX_COLS = 150
-TILE_SIZE = SCREEN_HEIGHT // ROWS
+TILE_WIDTH = 16
+TILE_HEIGHT = 16
+NUM_TILES = 48
+
+ROWS = SCREEN_HEIGHT // TILE_HEIGHT
+COLS = SCREEN_WIDTH // TILE_WIDTH
+MAX_COLS = 100
 
 clock = pygame.time.Clock()
 FPS = 30
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Level Designer')
+pygame.display.set_caption('Level Editor')
 
-# game_variables
+# game_variables **************************************************************
 scroll_left = False
 scroll_right = False
 scroll = 0
@@ -43,45 +47,44 @@ for row in range(ROWS):
 	col = [-1] * MAX_COLS
 	world_data.append(col)
 
-# populating last row with ground
-# for i in range(MAX_COLS):
-# 	world_data[ROWS-1][i] = 0
-
 # load images
-sky_img = pygame.image.load('assets/BG1.png')
-mountain_img = pygame.image.load('assets/mountain.png')
+sky = pygame.transform.scale(pygame.image.load('assets/BG1.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+mountain = pygame.transform.scale(pygame.image.load('assets/BG2.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+ground = pygame.transform.scale(pygame.image.load('assets/BG3.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 save_img = pygame.image.load('assets/save_btn.png')
 load_img = pygame.image.load('assets/load_btn.png')
 left_img = pygame.image.load('assets/left.png')
 right_img = pygame.image.load('assets/right.png')
 
+# load tileset
+
 img_list = []
-for i in range(1,30):
+for i in range(1,NUM_TILES+1):
 	img = pygame.image.load(f'tiles/{i}.png')
-	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+	# img = pygame.transform.scale(img, (TILE_WIDTH, TILE_SIZE))
 	img_list.append(img)
 
 
 def draw_bg():
 	win.fill(GREEN)
-	width = sky_img.get_width()
-	for x in range(4):
-		win.blit(sky_img, ((x*width)-scroll * 0.5,0))
-		win.blit(mountain_img, ((x*width)-scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height()  + 35))
+	win.blit(sky, (0,0))
+	win.blit(mountain, (0,-50))
+	win.blit(ground, (0,0))
 
 def draw_grid():
 	# horizontal lines
 	for c in range(ROWS + 1):
-		pygame.draw.line(win, WHITE, (0, c * TILE_SIZE), (SCREEN_WIDTH, c * TILE_SIZE))
+		pygame.draw.line(win, WHITE, (0, c * TILE_HEIGHT), (SCREEN_WIDTH, c * TILE_HEIGHT))
 	# vertical lines
 	for c in range(MAX_COLS + 1):
-		pygame.draw.line(win, WHITE, (c * TILE_SIZE - scroll, 0), (c * TILE_SIZE - scroll, SCREEN_HEIGHT))
+		pygame.draw.line(win, WHITE, (c * TILE_WIDTH - scroll, 0), (c * TILE_WIDTH - scroll, SCREEN_HEIGHT))
 
 def draw_world():
 	for y, row in enumerate(world_data):
 		for x, tile in enumerate(row):
 			if tile >= 0:
-				win.blit(img_list[tile], (x*TILE_SIZE - scroll, y*TILE_SIZE))
+				win.blit(img_list[tile], (x*TILE_WIDTH - scroll, y*TILE_HEIGHT))
 
 def draw_text(text_, font, color, pos):
 	text = font.render(text_, True, color)
@@ -92,25 +95,29 @@ button_list = []
 b_col = 0
 b_row = 0
 for i in range(len(img_list)):
-	t_button = button.Button(SCREEN_WIDTH + (50 * b_col + 30), 50 * b_row + 30, img_list[i], 1)
+	if TILE_WIDTH > 16:
+		image = pygame.transform.scale(img_list[i], (16,16))
+	else:
+		image = img_list[i]
+	t_button = button.Button(SCREEN_WIDTH + (35 * b_col + 15), 30 * b_row + 10, image, 1)
 	button_list.append(t_button)
 
 	b_col += 1
-	if b_col == 5:
+	if b_col == 8:
 		b_row += 1
 		b_col = 0
 
 # #create load and save buttons
-load_button = button.Button(SCREEN_WIDTH + 10, SCREEN_HEIGHT - 80, load_img, 1)
-save_button = button.Button(SCREEN_WIDTH + 110, SCREEN_HEIGHT - 80, save_img, 1)
-left_button = button.Button(SCREEN_WIDTH + 30, SCREEN_HEIGHT - 35, left_img, 1)
-right_button = button.Button(SCREEN_WIDTH + 140, SCREEN_HEIGHT - 35, right_img, 1)
+load_button = button.Button(SCREEN_WIDTH + 165, SCREEN_HEIGHT - 35, load_img, 0.7)
+save_button = button.Button(SCREEN_WIDTH + 235, SCREEN_HEIGHT - 35, save_img, 0.7)
+left_button = button.Button(SCREEN_WIDTH + 10, SCREEN_HEIGHT - 35, left_img, 1)
+right_button = button.Button(SCREEN_WIDTH + 120, SCREEN_HEIGHT - 35, right_img, 1)
 
 
 running = True
 while running:
 	draw_bg()
-	draw_grid()
+	# draw_grid()
 	draw_world()
 
 	# draw button panel
@@ -129,13 +136,13 @@ while running:
 	# map scroller
 	if scroll_left and scroll > 0:
 		scroll -= 5 * scroll_speed
-	if scroll_right and scroll < (MAX_COLS * TILE_SIZE) - SCREEN_WIDTH:
+	if scroll_right and scroll < (MAX_COLS * TILE_WIDTH) - SCREEN_WIDTH:
 		scroll += 5 * scroll_speed
 
 	# add new tiles
 	pos = pygame.mouse.get_pos()
-	x = ((pos[0] + scroll) // TILE_SIZE)
-	y = (pos[1] // TILE_SIZE)
+	x = ((pos[0] + scroll) // TILE_WIDTH)
+	y = (pos[1] // TILE_HEIGHT)
 
 	if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGHT:
 		if pygame.mouse.get_pressed()[0] == 1:
@@ -167,14 +174,18 @@ while running:
 
 	if save_button.draw(win):
 		#save level data
-		pickle_out = open(f'scrollable_levels/level{current_level}_data', 'wb')
+		pickle_out = open(f'levels/level{current_level}_data', 'wb')
 		pickle.dump(world_data, pickle_out)
 		pickle_out.close()
 	if load_button.draw(win):
 		#load in level data
-		if os.path.exists(f'scrollable_levels/level{current_level}_data'):
-			pickle_in = open(f'scrollable_levels/level{current_level}_data', 'rb')
-			world_data = pickle.load(pickle_in)
+		if os.path.exists(f'levels/level{current_level}_data'):
+			pickle_in = open(f'levels/level{current_level}_data', 'rb')
+			data = pickle.load(pickle_in)
+			for i in range(len(data)):
+				for j in range(len(data[0])):
+					world_data[i][j] = data[i][j]
+			del data
 
 	if left_button.draw(win):
 		current_level -= 1
@@ -184,7 +195,8 @@ while running:
 		current_level += 1
 
 	#text showing current level
-	draw_text(f'Level: {current_level}', font, WHITE, (SCREEN_WIDTH + 70, SCREEN_HEIGHT - 25))
+	draw_text(f'Level: {current_level}', font, WHITE, (SCREEN_WIDTH + 53, SCREEN_HEIGHT - 25))
+	pygame.draw.line(win, WHITE, (SCREEN_WIDTH+10, HEIGHT-50), (WIDTH-10, HEIGHT-50), 2)
 
 	clock.tick(FPS)
 	pygame.display.update()
