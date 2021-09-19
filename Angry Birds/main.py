d0e3e0f3e21f4ac11d6918252c51fffbae01f0e1
pyguru123 <pyguru123@gmail.com>
2021-09-19 22:40:37 +0530

    updated main

--- Angry Birds/main.py
+++ Angry Birds/main.py
@@ -5,6 +5,7 @@ import time
 import pygame
 import pymunk
 from characters import Bird, Pig
+from objects import SlingShot
 
 pygame.init()
 WIDTH, HEIGHT = 800, 480
@@ -22,13 +23,7 @@ BLUE = (0, 0, 255)
 bg = pygame.image.load('res/background3.png').convert_alpha()
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
 redbird_img = pygame.image.load('res/red-bird3.png').convert_alpha()
-sling_image = pygame.image.load('res/sling-3.png').convert_alpha()
 pig_image = pygame.image.load('res/pig.png')
-
-# FUNCTIONS *******************************************************************
-
-def convert_to_pygame(pos):
-	return int(pos.x), int(-pos.y + HEIGHT)
 
 # OBJECTS *********************************************************************
 
@@ -40,22 +35,21 @@ birds = []
 beams = []
 columns = []
 
+sling_x, sling_y = 135, 100
+sling = SlingShot(sling_x, sling_y)
+
 # FLOOR ***********************************************************************
 
 static_floor = pymunk.Body(body_type=pymunk.Body.STATIC)
-static_lines1 = [pymunk.Segment(static_floor, (0,60), (800, 60), 0)]
-static_lines2 = [pymunk.Segment(static_floor, (WIDTH- 10,60), (WIDTH-10, HEIGHT), 0)]
+static_lines = [pymunk.Segment(static_floor, (0,60), (800, 60), 0), 
+				pymunk.Segment(static_floor, (WIDTH- 10,60), (WIDTH-10, HEIGHT), 0)]
 
-for line in static_lines1:
-	line.elasticity = 0.95
-	line.friction = 1
-	line.collision_type = 3
-for line in static_lines2:
+for line in static_lines:
 	line.elasticity = 0.95
 	line.friction = 1
 	line.collision_type = 3
 
-space.add(static_floor, static_lines1, static_lines2)
+space.add(static_floor, *static_lines)
 
 # SLING ACTION ****************************************************************
 
@@ -69,8 +63,6 @@ mouse_y = 0
 mouse_pressed = False
 time_of_release = 0
 
-sling_x, sling_y = 135, 100
-
 running = True
 while running:
 	win.blit(bg, (0,0))
@@ -83,7 +75,7 @@ while running:
 			if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
 				running = False
 
-	win.blit(sling_image, (sling_x, sling_y))
+	sling.draw(win)
 
 	pygame.display.update()
 
