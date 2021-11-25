8be54a9207d86ca2e8feb8592e48382f0d8342da
pyguru123 <pyguru123@gmail.com>
2021-11-26 00:32:40 +0530

    added buttons

--- Corner Dash/main.py
+++ Corner Dash/main.py
@@ -8,7 +8,7 @@ import random
 import pygame
 
 from objects import Circle, Player, Dot, Particle, Snowflake, \
-					ScoreCard
+					ScoreCard, Button
  
 pygame.init()
 SCREEN = WIDTH, HEIGHT = 288, 512
@@ -43,14 +43,25 @@ color_list = [BLUE, GREEN, ORANGE, YELLOW]
 color_index = 3
 color = color_list[color_index]
 
-# IMAGES *********************************************************************
-
-main_circle = pygame.image.load('Assets/main.png')
-
 # FONTS **********************************************************************
 
 score_font = "Fonts/neuropol x rg.ttf"
 score_msg = ScoreCard(WIDTH//2, 60, 35, score_font, WHITE, win)
+
+# IMAGES *********************************************************************
+
+main_circle = pygame.image.load('Assets/main.png')
+
+close_img = pygame.image.load('Assets/closeBtn.png')
+replay_img = pygame.image.load('Assets/replay.png')
+sound_off_img = pygame.image.load("Assets/soundOffBtn.png")
+sound_on_img = pygame.image.load("Assets/soundOnBtn.png")
+
+# Buttons ********************************************************************
+
+close_btn = Button(close_img, (24, 24), WIDTH // 4 - 18, HEIGHT//2 + 120)
+replay_btn = Button(replay_img, (36,36), WIDTH // 2  - 18, HEIGHT//2 + 115)
+sound_btn = Button(sound_on_img, (24, 24), WIDTH - WIDTH // 4 - 18, HEIGHT//2 + 120)
 
 # GROUP & OBJECTS ************************************************************
 
@@ -64,7 +75,6 @@ for i in range(12):
 p = Player()
 d = Dot()
 pos = random.randint(0, 11)
-dot_circle = circle_group.sprites()[pos]
 
 # VARIABLES ******************************************************************
 
@@ -105,7 +115,32 @@ while running:
 		pass
 
 	if score_page:
-		pass
+		if close_btn.draw(win):
+			running = False
+
+		if replay_btn.draw(win):
+			clicks = 0
+			score = 0
+			pos = random.randint(0, 11)
+
+			p.reset()
+			# start_rotation = False
+			# score = 0
+			# final_score_msg = Message(144, HEIGHT//2-50, 100, "0",final_score_font, WHITE, win)
+
+			score_page = False
+			game_page = True
+
+		if sound_btn.draw(win):
+			pass
+			# sound_on = not sound_on
+			
+			# if sound_on:
+			# 	sound_btn.update_image(sound_on_img)
+			# 	pygame.mixer.music.play(loops=-1)
+			# else:
+			# 	sound_btn.update_image(sound_off_img)
+			# 	pygame.mixer.music.stop()
 
 	if game_page:
 		win.blit(main_circle, (CENTER[0] - 12.5, CENTER[1] - 12.5))
@@ -118,46 +153,53 @@ while running:
 		p.update(rotate)
 		p.draw(win)
 
-		if score and score % 7 == 0:
-			if score not in score_list:
-				score_list.append(score)
-				shrink = not shrink
+		if p.alive:
+			if score and score % 7 == 0:
+				if score not in score_list:
+					score_list.append(score)
+					shrink = not shrink
 
-		if clicks and clicks % 5 == 0:
-			color_index = (color_index + 1) % len(color_list)
-			color = color_list[color_index]
+			if clicks and clicks % 5 == 0:
+				color_index = (color_index + 1) % len(color_list)
+				color = color_list[color_index]
 
-			r = random.choice([-1, 1])
-			for c in circle_group:
-				c.dt *= -r
-				c.rotate = True
+				r = random.choice([-1, 1])
+				for c in circle_group:
+					c.dt *= -r
+					c.rotate = True
 
-			clicks = 0
+				clicks = 0
 
-		dot_circle = circle_group.sprites()[pos]
-		x, y = dot_circle.rect.center
-		d.update(x, y, win, color)
+			dot_circle = circle_group.sprites()[pos]
+			x, y = dot_circle.rect.center
+			d.update(x, y, win, color)
 
-		for circle in circle_group:
-			if circle.complete:
-				if pygame.sprite.collide_mask(p, circle):
-					if circle.i == pos:
-						pos = random.randint(0, 11)
+			for circle in circle_group:
+				if circle.complete:
+					if pygame.sprite.collide_mask(p, circle):
+						if circle.i == pos:
+							pos = random.randint(0, 11)
 
-						x, y = circle.rect.center
-						for i in range(10):
-							particle = Particle(x, y, color, win)
-							particle_group.add(particle)
+							x, y = circle.rect.center
+							for i in range(10):
+								particle = Particle(x, y, color, win)
+								particle_group.add(particle)
 
-						score += 1
-					p.dr *= -1
+							score += 1
+						p.dr *= -1
 
-		x, y = p.rect.center
-		if (x < 0 or x > WIDTH or y < 0 or y > HEIGHT):
-			for i in range(10):
-				particle = Particle(x, y, WHITE, win)
-				particle_group.add(particle)
-			p.reset()
+			x, y = p.rect.center
+			if (x < 0 or x > WIDTH or y < 0 or y > HEIGHT):
+				for i in range(10):
+					particle = Particle(x, y, WHITE, win)
+					particle_group.add(particle)
+				p.alive = False
+
+		if not p.alive and len(particle_group) == 0:
+			particle_group.empty()
+
+			game_page = False
+			score_page = True
 
 	pygame.draw.rect(win, WHITE, (0, 0, WIDTH, HEIGHT), 8)
 	clock.tick(FPS)
--- Corner Dash/objects.py
+++ Corner Dash/objects.py
@@ -207,3 +207,32 @@ class ScoreCard:
 		
 		self.win.blit(shadow, self.shadow_rect)
 		self.win.blit(self.image, self.rect)
+
+class Button(pygame.sprite.Sprite):
+	def __init__(self, img, scale, x, y):
+		super(Button, self).__init__()
+		
+		self.scale = scale
+		self.image = pygame.transform.scale(img, self.scale)
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+		self.clicked = False
+
+	def update_image(self, img):
+		self.image = pygame.transform.scale(img, self.scale)
+
+	def draw(self, win):
+		action = False
+		pos = pygame.mouse.get_pos()
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] and not self.clicked:
+				action = True
+				self.clicked = True
+
+			if not pygame.mouse.get_pressed()[0]:
+				self.clicked = False
+
+		win.blit(self.image, self.rect)
+		return action
