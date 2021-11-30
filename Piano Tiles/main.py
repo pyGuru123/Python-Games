2802397cdc48ad278bb939855a83ecf236410b9b
pyguru123 <pyguru123@gmail.com>
2021-11-30 19:23:56 +0530

    added piano tiles

---
+++ Piano Tiles/main.py
@@ -0,0 +1,40 @@
+# Piano Tiles
+
+# Author : Prajjwal Pathak (pyguru)
+# Date : Thursday, 30 November, 2021
+
+import pygame
+
+pygame.init()
+SCREEN = WIDTH, HEIGHT = 288, 512
+
+info = pygame.display.Info()
+width = info.current_w
+height = info.current_h
+
+if width >= height:
+	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
+else:
+	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
+
+clock = pygame.time.Clock()
+FPS = 60
+
+BLACK = (255, 255, 255)
+
+running = True
+while running:
+	win.fill(BLACK)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE or \
+				event.key == pygame.K_q:
+				running = False
+
+	clock.tick(FPS)
+	pygame.display.update()
+
+running = False
