a3dc5c71082f9c5257d93f9bdbdc571e02860dbf
pyguru123 <pyguru123@gmail.com>
2021-12-16 23:54:15 +0530

    removed gifs

--- Tetris/main.py
+++ Tetris/main.py
@@ -8,23 +8,21 @@ import pygame
 
 pygame.init()
 SCREEN = WIDTH, HEIGHT = 288, 512
-
-info = pygame.display.Info()
-width = info.current_w
-height = info.current_h
-
-if width >= height:
-	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
-else:
-	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
+win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
 
 clock = pygame.time.Clock()
 FPS = 60
 
+# COLORS *********************************************************************
+
+WHITE = (255, 255, 255)
+BLUE = (30, 144,255)
+BLACK = (0, 0, 20)
+
 
 running = True
 while running:
-	win.fill((255, 255, 255))
+	win.fill(BLACK)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
@@ -33,7 +31,7 @@ while running:
 			if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
 				running = False
 
-	pygame.draw.rect(win, (0,0,0), (0,0, WIDTH, HEIGHT), 5, border_radius=2)
+	pygame.draw.rect(win, BLUE, (0,0, WIDTH, HEIGHT), 5, border_radius=2)
 	clock.tick(FPS)
 	pygame.display.update()
 
