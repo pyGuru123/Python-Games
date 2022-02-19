61fde9f0f9b2994643b844833c67ee9b15a04ccc
pyguru123 <pyguru123@gmail.com>
2022-02-19 21:37:14 +0530

    completed hex dash

--- Hex Dash/main.py
+++ Hex Dash/main.py
@@ -7,7 +7,7 @@ import math
 import random
 import pygame
 
-from objects import Line, Player, Ball, Particle, Message
+from objects import Line, Player, Ball, Particle, Message, Button
 
 pygame.init()
 SCREEN = WIDTH, HEIGHT = 288, 512
@@ -36,15 +36,30 @@ PURPLE = (155,38,182)
 AQUA = (0,103,127)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-GRAY = (218, 218, 218)
+GRAY = (239, 237, 238)
+GRAY2 = (200, 200, 203)
+GRAY3 = (180, 180, 180)
 
-COLORS = [RED, GREEN, BLUE, PURPLE]
+COLORS = [RED, GREEN, BLUE, YELLOW, PURPLE]
 cindex = 0
 ccolor = COLORS[cindex]
 
 # Fonts
 score_font = "Fonts/DroneflyRegular-K78LA.ttf"
-score_msg = Message(WIDTH//2, HEIGHT//2, 60, "0", score_font, (60, 60, 60), win)
+msg_font = "Fonts/Biondi Sans.ttf"
+
+game_msg = Message(90, HEIGHT//2+20, 30, "GAME", msg_font, BLACK, win)
+over_msg = Message(WIDTH//2+60, HEIGHT//2+20, 30, "OVER!", msg_font, RED, win)
+
+cscore_msg = Message(WIDTH//2, 50, 20, "SCORE", msg_font, GRAY3, win)
+cscore_msg2 = Message(WIDTH//2, 75, 24, "0", msg_font, BLACK, win)
+best_msg = Message(WIDTH//2, 140, 20, "BEST", msg_font, GRAY3, win)
+bestscore_msg = Message(WIDTH//2, 170, 24, "0", msg_font, BLACK, win)
+
+hex_msg = Message(WIDTH//2, HEIGHT//2-30, 40, "HEX", msg_font, GRAY3, win)
+dash_msg = Message(WIDTH//2, HEIGHT//2+20, 40, "DASH", msg_font, GRAY3, win)
+
+score_msg = Message(WIDTH//2, HEIGHT//2, 60, "0", score_font, GRAY2, win)
 
 # Sounds
 score_fx = pygame.mixer.Sound('Sounds/click.wav')
@@ -55,6 +70,16 @@ explode_fx = pygame.mixer.Sound('Sounds/explode.wav')
 pygame.mixer.music.load('Sounds/music.wav')
 pygame.mixer.music.play(loops=-1)
 pygame.mixer.music.set_volume(0.7)
+
+# Buttons
+close_img = pygame.image.load('Assets/closeBtn.png')
+replay_img = pygame.image.load('Assets/replay.png')
+sound_off_img = pygame.image.load("Assets/soundOffBtn.png")
+sound_on_img = pygame.image.load("Assets/soundOnBtn.png")
+
+close_btn = Button(close_img, (24, 24), WIDTH // 4 - 18, HEIGHT//2 + 120)
+replay_btn = Button(replay_img, (36,36), WIDTH // 2  - 18, HEIGHT//2 + 115)
+sound_btn = Button(sound_on_img, (24, 24), WIDTH - WIDTH // 4 - 18, HEIGHT//2 + 120)
 
 # Positons 
 left = 100
@@ -91,8 +116,16 @@ bar = Line((l.x1, l.y1), (l.x2, l.y2))
 
 clicked = False
 score = 0
+high = 0
 counter = 0
+
+sound_on = True
 gameover = False
+home_page = True
+game_page = False
+score_page = False
+
+color = COLORS[random.randint(0,4)]
 
 running = True
 while running:
@@ -108,49 +141,95 @@ while running:
         	running = False
         	
         if event.type == pygame.MOUSEBUTTONDOWN:
-        	if not clicked:
+        	if not clicked and game_page:
         		clicked = True
         		player.di *= -1
         		player.update_index()
         		rect = line_group.sprites()[player.index]
         		
+        	if home_page:
+        		home_page = False
+        		game_page = True
+        		
         if event.type == pygame.MOUSEBUTTONUP:
         	clicked = False
+        	
+    if home_page:
+        	line_group.update(win, color)
+        	hex_msg.update()
+        	dash_msg.update()
+        	
+    if score_page:
+    	cscore_msg.update(shadow=False)
+    	cscore_msg2.update(score, shadow=False)
+    	best_msg.update(shadow=False)
+    	bestscore_msg.update(high, shadow=False)
+    	game_msg.update(shadow=False)
+    	over_msg.update(shadow=False)
+    	
+    	if close_btn.draw(win):
+    		running = False
+    		
+    	if replay_btn.draw(win):
+    		ball_group.empty()
+    		gameover = False
+    		score = 0
+    		
+    		player = Player(WIDTH//2, HEIGHT//2)
+    		score_msg = Message(WIDTH//2, HEIGHT//2, 60, "0", score_font, GRAY2, win)
+    		
+    		game_page = True
+    		score_page = False
+    		home_page = False
+    	if sound_btn.draw(win):
+    		sound_on = not sound_on
+    		
+    		if sound_on:
+    			sound_btn.update_image(sound_on_img)
+    			pygame.mixer.music.play(loops=-1)
+    		else:
+    			sound_btn.update_image(sound_off_img)
+    			pygame.mixer.music.stop()
     
     # drawing Objects
-    score_msg.update(score)
-    line_group.update(win)
-    particle_group.update()
-    bar.update(win, ccolor)
-    line = line_group.sprites()[player.index]
-    player.update(line, ccolor, win)
-    ball_group.update(win)
-    
-    # Collison Detection
-    if player.rect.collidepoint(bar.get_center()):
-    	bar_index = random.randint(0,5)
-    	l= line_group.sprites()[bar_index]
-    	bar = Line((l.x1, l.y1), (l.x2, l.y2))
-    	
-    	score_fx.play()
-    	score += 1
-    	if score % 3 == 0:
-    		cindex = (cindex + 1) % 3
-    		ccolor = COLORS[cindex]
-    		
-    for ball in ball_group:
-    	if player.alive and ball.rect.colliderect(player.rect):
-    		x, y = player.rect.centerx, player.rect.centery
-    		for i in range(20):
-    			particle = Particle(x, y, ccolor, win)
-    			particle_group.add(particle)
-    			gameover = True
-    		player.alive = False
-    		ball.kill()
-    		explode_fx.play()
-    			
-    if gameover and len(particle_group) == 0:
-    		running = False
+    if game_page:
+	    score_msg.update(score, shadow=True)
+	    line_group.update(win)
+	    particle_group.update()
+	    bar.update(win, ccolor)
+	    line = line_group.sprites()[player.index]
+	    player.update(line, ccolor, win)
+	    ball_group.update(win)
+	    
+	    # Collison Detection
+	    if player.rect.collidepoint(bar.get_center()):
+	    	bar_index = random.randint(0,5)
+	    	l= line_group.sprites()[bar_index]
+	    	bar = Line((l.x1, l.y1), (l.x2, l.y2))
+	    	
+	    	score_fx.play()
+	    	score += 1
+	    	if score & score > high:
+	    		high = score
+	    	if score % 3 == 0:
+	    		cindex = (cindex + 1) % 5
+	    		ccolor = COLORS[cindex]
+	    		
+	    for ball in ball_group:
+	    	if player.alive and ball.rect.colliderect(player.rect):
+	    		x, y = player.rect.centerx, player.rect.centery
+	    		for i in range(20):
+	    			particle = Particle(x, y, ccolor, win)
+	    			particle_group.add(particle)
+	    		player.alive = False
+	    		ball.kill()
+	    		if not gameover:
+	    			explode_fx.play()
+	    			gameover = True
+	    			
+	    if gameover and len(particle_group) == 0:
+	    		game_page = False
+	    		score_page = True
     
     pygame.draw.rect(win, BLUE, (0,0,WIDTH-2, HEIGHT-2), 2)
     
