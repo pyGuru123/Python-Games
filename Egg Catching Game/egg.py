81c68b88b3c59f487f54b3748b7d31f82c8e1c15
Prajjwal Pathak <71623767+pyGuru123@users.noreply.github.com>
2022-07-23 23:18:36 +0530

    Add files via upload

---
+++ Egg Catching Game/egg.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+
+pygame.init()
+SCREEN = WIDTH, HEIGHT = 288, 512
+
+info = pygame.display.Info()
+width = info.current_w
+height = info.current_h
+
+if width >= height:
+	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
+else:
+	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
+
+clock = pygame.time.Clock()
+FPS = 45
+
+# COLORS
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (54, 69, 79)
+c_list = [RED, BLACK, WHITE]
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				running = False
+				
+	clock.tick(FPS)
+	pygame.display.update()
+	
+pygame.quit()
