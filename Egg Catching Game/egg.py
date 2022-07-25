b3e9dfae6949395b1d9804fa8c946eefe4788b79
Prajjwal Pathak <71623767+pyGuru123@users.noreply.github.com>
2022-07-25 22:45:25 +0530

    Updated Egg

--- Egg Catching Game/egg.py
+++ Egg Catching Game/egg.py
@@ -24,8 +24,17 @@ BLACK = (0, 0, 0)
 GRAY = (54, 69, 79)
 c_list = [RED, BLACK, WHITE]
 
+# IMAGES
+
+bg = pygame.image.load("Assets/bg.png")
+bg = pygame.transform.scale(bg, (WIDTH, HEIGHT+50))
+
+egg_bucket = pygame.image.load("Assets/egg_bucket.png")
+egg_bucket = pygame.transform.scale(egg_bucket, (120, 80))
+
 running = True
 while running:
+	win.blit(bg, (0,0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
@@ -34,7 +43,10 @@ while running:
 			if event.key == pygame.K_ESCAPE:
 				running = False
 				
+	#win.blit(egg_bucket, (WIDTH//2, HEIGHT-120))
+				
 	clock.tick(FPS)
+	pygame.draw.rect(win, WHITE, (0,0, WIDTH, HEIGHT), 2)
 	pygame.display.update()
 	
 pygame.quit()
